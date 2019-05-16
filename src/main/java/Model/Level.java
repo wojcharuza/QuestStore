61f80973d06a1fd027a892a4c5301b253600b7c9
Paweł Płodzień <pawel.plodzien@gmail.com>
2019-05-16 10:05:16 +0200

    delete level, edit levels

---
+++ src/main/java/Model/Level.java
@@ -0,0 +1,45 @@
+package Model;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+public class Level {
+  private int levelNumber;
+  private int experienceNeeded;
+
+  public Level (Builder builder){
+    this.levelNumber = builder.levelNumber;
+    this.experienceNeeded = builder.experienceNeeded;
+  }
+
+  public int getLevelNumber() {
+    return levelNumber;
+  }
+
+  public int getExperienceNeeded() {
+    return experienceNeeded;
+  }
+
+
+  public static class Builder {
+    private int levelNumber;
+    private int experienceNeeded;
+
+
+
+    public Builder withLevelNumber(int levelNumber) {
+      this.levelNumber = levelNumber;
+      return this;
+    }
+
+    public Builder withExperienceNeeded(int experienceNeeded) {
+      this.experienceNeeded = experienceNeeded;
+      return this;
+
+    }
+
+    public Level build(){
+      return new Level(this);
+    }
+  }
+}
