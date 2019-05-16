ea44e944a10f44056bf7740f0f05d63de3dfbdf1
Paweł Płodzień <pawel.plodzien@gmail.com>
2019-05-16 14:45:13 +0200

    get Students used artifacts

--- src/main/java/Dao/TransactionDaoImpl.java
+++ src/main/java/Dao/TransactionDaoImpl.java
@@ -2,20 +2,44 @@ package Dao;
 
 import Model.Card;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionDaoImpl implements TransactionDao {
 
 
-    public List<Card> getCardsUsedByStudent() {
+    public List<Card> getCardsUsedByStudent(int studentId) throws DaoException {
+        List<Card> usedCards = new ArrayList<>();
+        try(Connection con = C3P0DataSource.getInstance().getConnection()){
+            PreparedStatement stmt = null;
+            stmt = con.prepareStatement("SELECT title, description, image_path, card_type, " +
+                                            "coolcoin_value FROM \"Transactions\" LEFT JOIN \"Cards\" ON " +
+                                            "\"Transactions\".card_title = \"Cards\".title WHERE student_id = ? AND " +
+                                            "card_type::text LIKE 'artifact%' ;");
+            stmt.setInt(1, studentId);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                String title = rs.getString("title");
+                String description = rs.getString("description");
+                String imagePath = rs.getString("image_path");
+                String card_type = rs.getString("card_type");
+                int coolcoinValue = rs.getInt("coolcoin_value");
+                Card card = new Card.Builder().withTitle(title).withDescription(description)
+                        .withImagePath(imagePath).withCardType(card_type).withCoolcoinValue(coolcoinValue).build();
+                usedCards.add(card);
+            }
+            return usedCards;
 
-
-
-
-        return null;
+        } catch (SQLException e) {
+            throw new DaoException();
+        }
     }
 
-    public void markQuestCompletedByStudent() {
+
+
+
+    public void markQuestCompletedByStudent() throws DaoException {
 
     }
 }
